be9a430cace2de525865ab23addbc74f5e9c3b55
Christopher Robinson <chriserino210@protonmail.com>
2025-12-24 14:14:59 -0600

    relocated icons to inside images for conformity and consistency

--- frontend/src/constants/experience.ts
+++ frontend/src/constants/experience.ts
@@ -2,27 +2,27 @@ export const experience = [
     {
         title: 'Front End',
         skills: [
-            { name: 'HTML5', icon: '/icons/HTML5_Logo_64.png' },
-            { name: 'CSS', icon: '/icons/CSS3_Logo_64.png' },
-            { name: 'JavaScript', icon: '/icons/JS_Logo_64.png' },
+            { name: 'HTML5', icon: '/images/icons/HTML5_Logo_64.png' },
+            { name: 'CSS', icon: '/images/icons/CSS3_Logo_64.png' },
+            { name: 'JavaScript', icon: '/images/icons/JS_Logo_64.png' },
         ]
     },
     {
         title: 'Back End',
         skills: [
-            { name: 'PHP', icon: '/icons/php-original.svg' },
-            { name: 'SQL', icon: '/icons/microsoftsqlserver-plain.svg' },
-            { name: 'MySQL', icon: '/icons/mysql-original.svg' },
+            { name: 'PHP', icon: '/images/icons/php-original.svg' },
+            { name: 'SQL', icon: '/images/icons/microsoftsqlserver-plain.svg' },
+            { name: 'MySQL', icon: '/images/icons/mysql-original.svg' },
             { name: 'API', icon: '' },
         ]
     },
     {
         title: 'Framework',
         skills: [
-            { name: 'React', icon: '/icons/react-original.svg' },
-            { name: 'Node.js', icon: '/icons/nodejs-original.svg' },
-            { name: 'Next.js', icon: '/icons/next-js.png' },
-            { name: 'Laravel', icon: '/icons/laravel.png' }
+            { name: 'React', icon: '/images/icons/react-original.svg' },
+            { name: 'Node.js', icon: '/images/icons/nodejs-original.svg' },
+            { name: 'Next.js', icon: '/images/icons/next-js.png' },
+            { name: 'Laravel', icon: '/images/icons/laravel.png' }
         ]
     }
 ];
