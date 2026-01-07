b0ed14a4b89f4556389eee313c1733c28f6e65b7
Christopher Robinson <chriserino210@protonmail.com>
2026-01-07 14:23:10 -0600

    Add AddProjects component for project editing interface, featuring a Card and TextInput for user input

--- frontend/src/components/pages/admin/AddProjects.tsx
+++ frontend/src/components/pages/admin/AddProjects.tsx
@@ -1,12 +1,13 @@
 import Card from "../../common/Card"
-
-export default function EditProjects() {
+import TextInput from "../../common/TextInput"
+export default function AddProjects() {
     return (
         <div className="flex-col justify-center items-center min-h-screen p-4">
             <div>Edit Projects</div>
             <Card>
                 <div>TEST CARD</div>
                 <div>Something else</div>
+                <div><TextInput /></div>
             </Card>
         </div>
     )
