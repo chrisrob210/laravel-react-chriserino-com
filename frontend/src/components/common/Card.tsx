be723f6485638798c3931d2fa1b8289824d4c3e8
Christopher Robinson <chriserino210@protonmail.com>
2026-01-07 14:22:36 -0600

    Enhance Card component to include optional title prop for improved UI presentation

--- frontend/src/components/common/Card.tsx
+++ frontend/src/components/common/Card.tsx
@@ -3,11 +3,15 @@ import React from 'react'
 interface CardProps {
     children?: React.ReactNode;
     className?: string;
+    title?: string;
 }
 
-export default function Card({ children, className = '' }: CardProps) {
+export default function Card({ children, className = '', title }: CardProps) {
     return (
-        <div className='flex flex-col justify-between gap-3 w-auto px-6 py-4 border-[1px] rounded-lg border-white/20 shadow-md shadow-gray-500/50 bg-white/40 backdrop-blur-sm h-full'>
+        <div className='flex flex-col justify-center items-center gap-3 w-auto px-6 py-4 border-[1px] rounded-lg border-white/20 shadow-md shadow-gray-500/50 bg-white/40 backdrop-blur-sm h-full'>
+            {title &&
+                <div className='text-xs font-bold text-center px-1 py-[0.1rem] rounded text-red-600'>{title}</div>
+            }
             {children}
         </div>
     )
