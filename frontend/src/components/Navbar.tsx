9b70ed28d61e42c4c330e8fdd4c719c1950857c1
Christopher Robinson <chriserino210@protonmail.com>
2026-01-07 13:23:59 -0600

    Enhance Navbar component to include user authentication status and admin indication, filtering routes based on admin privileges

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { routes } from '../constants/routes'
+import { useUser, SignInButton, UserButton } from '@clerk/clerk-react'
+import { useAdmin } from '../hooks/useAdmin'
 
 export default function Navbar() {
     const location = useLocation();
-
+    const { isSignedIn } = useUser();
+    const isAdmin = useAdmin();
     return (
         <nav className="bg-white shadow-lg p-0 m-0">
-            <div className="relative flex items-center justify-center py-4 px-3">
-                {/* <div className="absolute left-3 text-xl font-bold">Chriserino.com</div> */}
-                <div className="absolute left-3">
+            <div className="relative flex items-center justify-between py-4 px-3">
+                <div className='pl-3'>
                     <img
                         src="/c-logo32.png"
                         alt="Chriserino.com"
@@ -19,6 +21,11 @@ export default function Navbar() {
                     {routes
                         .filter(route => route.label) // Only show routes with labels
                         .filter(route => route.showInNavbar === true)
+                        .filter(route => {
+                            // If route is protected (admin), only show if user is admin
+                            // Otherwise, show the route
+                            return route.protected ? isAdmin : true;
+                        })
                         .map(route => (
                             <Link
                                 key={route.path}
@@ -31,6 +38,15 @@ export default function Navbar() {
                                 {route.label}
                             </Link>
                         ))}
+                </div>
+                <div className='pr-3 flex justify-center items-center'>
+                    {isAdmin && (
+                        <span className="text-[0.5rem] bg-red-500 text-white px-1 py-[0.1rem] mr-1 rounded">
+                            Admin
+                        </span>
+                    )}
+                    {!isSignedIn && <SignInButton />}
+                    {isSignedIn && <UserButton />}
                 </div>
             </div>
         </nav>
