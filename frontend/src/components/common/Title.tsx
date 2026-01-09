62b80c6e08e9155a8fee14d81c56bb560a29e0de
Christopher Robinson <chriserino210@protonmail.com>
2026-01-08 19:38:14 -0600

    Add Title component for displaying titles with customizable text styling

---
+++ frontend/src/components/common/Title.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export interface TitleProps {
+    title: string
+}
+
+export default function Title({ title }: TitleProps) {
+    return (
+        <div className="text-red-600 font-semibold w-fit mx-auto p-5">{title}</div>
+    )
+}
